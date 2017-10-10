d152ec502fb1a93e80e4b589d642efdb5301f7ca
Leonardo Dionizio <leodionizio13@gmail.com>
2017-10-10 12:37:43 -0300

    add home module, add page home, add list of annotations

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,36 @@
+import { AnnotationModel } from './../../models/annotation.model';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, IonicPage } from 'ionic-angular';
 
+import { AnnotationProvider } from './../../providers/annotation/annotation';
+
+@IonicPage()
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public annotations: AnnotationModel[] = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(
+    public navCtrl: NavController,
+    public annotationProvider: AnnotationProvider
+  ) {
 
   }
 
+  ionViewDidLoad() {
+    this.annotationProvider.getAll()
+      .then((annotations: AnnotationModel[]) => {
+        this.annotations = annotations
+      })
+  }
+
+  public viewAnnotation(annotation?: AnnotationModel): void {
+    if (!annotation) {
+      this.navCtrl.push("AnnotationPage");
+    } else {
+      this.navCtrl.push("AnnotationPage", annotation);
+    }
+  }
 }
