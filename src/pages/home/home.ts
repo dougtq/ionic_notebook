09bfab25691b52f31b615efa6d12a01e0a2b7310
Leonardo Dionizio <leodionizio13@gmail.com>
2017-10-11 15:04:48 -0300

    att home page

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,7 +2,7 @@ import { AnnotationModel } from './../../models/annotation.model';
 import { Component } from '@angular/core';
 import { NavController, IonicPage } from 'ionic-angular';
 
-import { AnnotationProvider } from './../../providers/annotation/annotation';
+import { AnnotationProvider } from './../../providers/annotation/annotation.service';
 
 @IonicPage()
 @Component({
@@ -15,11 +15,13 @@ export class HomePage {
   constructor(
     public navCtrl: NavController,
     public annotationProvider: AnnotationProvider
-  ) {
-
-  }
+  ) { }
 
   ionViewDidLoad() {
+    this.getAnnotations();
+  }
+
+  private getAnnotations(): void {
     this.annotationProvider.getAll()
       .then((annotations: AnnotationModel[]) => {
         this.annotations = annotations
@@ -28,9 +30,9 @@ export class HomePage {
 
   public viewAnnotation(annotation?: AnnotationModel): void {
     if (!annotation) {
-      this.navCtrl.push("AnnotationPage");
+      this.navCtrl.setRoot("AnnotationPage");
     } else {
-      this.navCtrl.push("AnnotationPage", annotation);
+      this.navCtrl.setRoot("AnnotationPage", { annotation });
     }
   }
 }
